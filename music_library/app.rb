25c146755c71a19b7bf0240374ae1851e4cee694
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-09-06 16:38:19 +0100

    Album repository

--- music_library/app.rb
+++ music_library/app.rb
@@ -2,20 +2,23 @@
 
 require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
+require_relative 'lib/albums_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('music_library')
 
-# # Perform a SQL query on the database and get the result set.
+# Perform a SQL query on the database and get the result set.
 # sql = 'SELECT id, title FROM albums;'
 # result = DatabaseConnection.exec_params(sql, [])
 
-# # Print out each record from the result set .
-# result.each do |record|
-#   p record
-# end
 artist_repository = ArtistRepository.new
 
 artist_repository.all.each do |record|
   puts "#{record.id} - #{record.name} - #{record.genre}"
+end
+
+album_repository = AlbumRepository.new
+
+album_repository.all.each do |record|
+  puts "#{record.id} - #{record.title} - #{record.release_year} - #{record.artist_id}"
 end
---
+++ music_library/lib/albums_repository.rb
@@ -0,0 +1,19 @@
+require_relative 'albums'
+
+class AlbumRepository
+    def all
+        albums = []
+        sql = 'SELECT id, title, release_year, artist_id FROM albums;'
+        result = DatabaseConnection.exec_params(sql, [])
+   
+        result.each do |record|
+         album = Album.new
+         album.id = record['id']
+         album.title = record['title']
+         album.release_year = record['release_year']
+         album.artist_id = record['artist_id']
+         albums << album
+        end
+        albums
+    end
+  end
