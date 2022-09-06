6325466b006834b8e572833b63aa5550967e55d7
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-09-06 15:21:37 +0100

    Artists repository

--- music_library/app.rb
+++ music_library/app.rb
@@ -1,15 +1,21 @@
 # file: app.rb
 
 require_relative 'lib/database_connection'
+require_relative 'lib/artist_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('music_library')
 
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
+# # Perform a SQL query on the database and get the result set.
+# sql = 'SELECT id, title FROM albums;'
+# result = DatabaseConnection.exec_params(sql, [])
 
-# Print out each record from the result set .
-result.each do |record|
-  p record
+# # Print out each record from the result set .
+# result.each do |record|
+#   p record
+# end
+artist_repository = ArtistRepository.new
+
+artist_repository.all.each do |record|
+  puts "#{record.id} - #{record.name} - #{record.genre}"
 end
