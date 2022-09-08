f14b48deabb9d2645178a20510bcb05c51a1294d
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-09-08 11:54:06 +0100

    App file updated

--- music_library/app.rb
+++ music_library/app.rb
@@ -1,45 +1,48 @@
-# file: app.rb
-
 require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
 require_relative 'lib/albums_repository'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+class Application
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect('music_library')
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+    @choice = []
+  end
 
-# Perform a SQL query on the database and get the result set.
-# sql = 'SELECT id, title FROM albums;'
-# result = DatabaseConnection.exec_params(sql, [])
-
-artist_repository = ArtistRepository.new
-
-artist_repository.all.each do |record|
-  puts "#{record.id} - #{record.name} - #{record.genre}"
+  def run
+    @io.puts "Welcome to the music library manager!"
+    @io.puts "What would you like to do?"
+    @io.puts "1 - List all albums"
+    @io.puts "2 - List all artists"
+    @io.puts "Enter your choice:"
+    @choice = @io.gets.chomp
+    @io.puts "Here is the list of albums:"
+    self.output
+  end
+  
+  def output
+   if @choice == "1"
+        album_repository = AlbumRepository.new
+        album_repository.all.each do |record|
+            @io.puts " * #{record.id} - #{record.title}"
+        end
+    elsif @choice == "2"
+        artist_repository = ArtistRepository.new
+        artist_repository.all.each do |record|
+            @io.puts "* #{record.id} - #{record.name}"
+        end
+    end
+  end
 end
 
-# artist_repository.all.each do |record|
-#   p record
-# end
-
-album_repository = AlbumRepository.new
-
-album_repository.all.each do |record|
-  puts "#{record.id} - #{record.title} - #{record.release_year} - #{record.artist_id}"
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
 end
-
-# album_repository.all.each do |record|
-#   p record
-# end
-
-# Get the album with id 3
-album = album_repository.find(3)
-
-puts album.id
-puts album.title
-puts album.release_year
-
-artist = artist_repository.find(2)
-
-puts artist.id
-puts artist.name
-puts artist.genre
